1fd0853605d66a92a780b21762ba8023d5608e7b
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2024-08-17 16:31:44 -0300

    Criei o Custom hook useFetch, para poder fazer a requisição na API.

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  const request = React.useCallback(async (url, options) => {
+    let json = null;
+    try {
+      setLoading(true);
+      setData(null);
+      setError(null);
+      const response = await fetch(url, options);
+      if (!response.ok) throw new Error(`Error: ${response.status}`);
+      json = await response.json();
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e.message);
+      } else {
+        setError('Error: erro ao fazer requisição');
+      }
+    } finally {
+      setLoading(false);
+      setData(json);
+    }
+  }, []);
+  return { data, error, loading, request };
+};
+
+export default useFetch;
